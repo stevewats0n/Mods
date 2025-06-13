8b67c774094cd63cb974415e6513079d3c1831cc
stevewats0n <88929858+stevewats0n@users.noreply.github.com>
2025-06-13 23:54:21 +0100

    Update Client_PresentSettingsUI.lua

--- ArmyUpkeep/Client_PresentSettingsUI.lua
+++ ArmyUpkeep/Client_PresentSettingsUI.lua
@@ -1,6 +1,8 @@
 
 function Client_PresentSettingsUI(rootParent)
-    UI.CreateLabel(rootParent).setText("Army cost per turn: "..Mod.Settings.armyCost)
+    UI.CreateLabel(rootParent)
+        .setText("Army cost per turn: "..
+        Mod.Settings.armyCost)
     --UI.CreateLabel(rootParent).setText("Armies destroyed if not enough gold? "..Mod.Settings.destroyArmies)
 
 end
