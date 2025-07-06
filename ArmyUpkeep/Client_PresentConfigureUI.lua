a203f4fe199702af870d00b5788b3a2e3ceacf86
stevewats0n <88929858+stevewats0n@users.noreply.github.com>
2025-07-06 12:54:22 +0100

    Update Client_PresentConfigureUI.lua

--- ArmyUpkeep/Client_PresentConfigureUI.lua
+++ ArmyUpkeep/Client_PresentConfigureUI.lua
@@ -10,6 +10,8 @@ function Client_PresentConfigureUI(rootParent)
     local initial_destroyArmies = Mod.Settings.destroyArmies;
     if initial_destroyArmies == nil then initial_destroyArmies = true end
 
+    local initial_setZeroArmiesNeutral = Mod.Settings.setZeroArmiesNeutral;
+
     local select = UI.CreateVerticalLayoutGroup(rootParent);
     UI.CreateLabel(select).SetText('Army cost per turn')
     armyCostInput = UI.CreateNumberInputField(select)
@@ -20,11 +22,11 @@ function Client_PresentConfigureUI(rootParent)
 
     select = UI.CreateVerticalLayoutGroup(rootParent);
     destroyArmiesInput = UI.CreateCheckBox(select).SetText("Destroy armies if not enough gold?")
-        .SetIsChecked(true)
+        .SetIsChecked(initial_destroyArmies)
 
     select = UI.CreateVerticalLayoutGroup(rootParent);
     setZeroArmiesNeutral = UI.CreateCheckBox(select).SetText("Set territories with 0 armies (and no special units) at end of the turn to neutral?")
-        .SetIsChecked(false)
+        .SetIsChecked(initial_setZeroArmiesNeutral)
 
 
 
