31b5c27ece5f2f7599a644c0df1310d54fb4fea7
stevewats0n <88929858+stevewats0n@users.noreply.github.com>
2025-07-06 11:58:34 +0100

    Update Client_SaveConfigureUI.lua

--- ArmyUpkeep/Client_SaveConfigureUI.lua
+++ ArmyUpkeep/Client_SaveConfigureUI.lua
@@ -2,5 +2,6 @@
 function Client_SaveConfigureUI (alert)
   Mod.Settings.armyCost = armyCostInput.GetValue()
   Mod.Settings.destroyArmies = destroyArmiesInput.GetIsChecked()
+  Mod.Settings.setZeroArmiesNeutral = setZeroArmiesNeutral.GetIsChecked()
   Mod.Settings.Version = 1
 end
