6d6fbc00d960401c6c640f683cf2d75bcd89cb95
stevewats0n <88929858+stevewats0n@users.noreply.github.com>
2025-07-06 11:57:33 +0100

    Update Client_PresentSettingsUI.lua

--- ArmyUpkeep/Client_PresentSettingsUI.lua
+++ ArmyUpkeep/Client_PresentSettingsUI.lua
@@ -4,5 +4,8 @@ function Client_PresentSettingsUI(rootParent)
     local destroy = Mod.Settings.destroyArmies
     if destroy then destroy = "true" else destroy = "false" end
     UI.CreateLabel(rootParent).SetText("Armies destroyed if not enough gold? "..destroy)
+    local setZeroArmiesNeutral = Mod.Settings.setZeroArmiesNeutral
+    if setZeroArmiesNeutral then setZeroArmiesNeutral = "true" else setZeroArmiesNeutral = "false" end
+    UI.CreateLabel(rootParent).SetText("Territory set to neutral if there are 0 armies (or special units) on it at the end of the turn? "..setZeroArmiesNeutral)
 
 end
