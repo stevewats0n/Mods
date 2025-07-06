cd91b151e70b1af45123011007661034775039af
stevewats0n <88929858+stevewats0n@users.noreply.github.com>
2025-07-06 22:16:57 +0100

    Update Client_SaveConfigureUI.lua

--- ArmyUpkeep/Client_SaveConfigureUI.lua
+++ ArmyUpkeep/Client_SaveConfigureUI.lua
@@ -2,6 +2,7 @@
 function Client_SaveConfigureUI (alert)
   if (armyCostInput.GetValue() * 100) % 1 ~= 0 then alert("Army cost should be a whole number or to one decimal place") end
   Mod.Settings.armyCost = armyCostInput.GetValue()
+  Mod.Settings.allowRemoveArmies = allowRemoveArmies.GetIsChecked()
   Mod.Settings.destroyArmies = destroyArmiesInput.GetIsChecked()
   Mod.Settings.setZeroArmiesNeutral = setZeroArmiesNeutral.GetIsChecked()
   Mod.Settings.Version = 1
