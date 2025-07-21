c160a4649f1f29989b818c64791c2f06c6018f1b
stevewats0n <88929858+stevewats0n@users.noreply.github.com>
2025-07-21 10:36:58 +0100

    Update Server_AdvanceTurn.lua

--- ArmyUpkeep/Server_AdvanceTurn.lua
+++ ArmyUpkeep/Server_AdvanceTurn.lua
@@ -5,27 +5,28 @@ require("Functions")
 function Server_AdvanceTurn_Order (Game, Order, Result, skipThisOrder, addNewOrder)
     if (Order.proxyType == "GameOrderCustom" and string.sub(Order.Payload, 1, 6) == "armyRM") then
         print (Order.Payload);
-        local ind = string.find(Order.Payload, ",")
-        local t_id = string.sub(Order.Payload, 7, ind-1)
+            addNewOrder(WL.GameOrderEvent.Create(Order.PlayerID, Order.Payload, {}, nil))
+--        local ind = string.find(Order.Payload, ",")
+--        local t_id = string.sub(Order.Payload, 7, ind-1)
         -- the actual count of armies
-        local army_count = Game.ServerGame.LatestTurnStanding.Territories[t_id].NumArmies.NumArmies
+--        local army_count = Game.ServerGame.LatestTurnStanding.Territories[t_id].NumArmies.NumArmies
         -- how much user requested to remove
-        local army_rm = string.sub(Order.Payload, ind+1)
+--        local army_rm = string.sub(Order.Payload, ind+1)
         -- but if the army count has reduced, we cannot have player ending with negative armies
-        army_rm = math.min(army_rm, army_count)
+--        army_rm = math.min(army_rm, army_count)
         -- this is if they try to give themselves extra armies;
-        if army_rm < 0 then army_rm = 0; end;
+--        if army_rm < 0 then army_rm = 0; end;
 
-        if Order.PlayerID ~= Game.ServerGame.LatestTurnStanding.Territories[t_id].OwnerPlayerID then
+--        if Order.PlayerID ~= Game.ServerGame.LatestTurnStanding.Territories[t_id].OwnerPlayerID then
             skipThisOrder(WL.ModOrderControl.Skip); return;
-        end
+--        end
 
-        local terr_mod = WL.TerritoryModification.Create(t_id)
-        terr_mod.AddArmies = -army_rm
-        local order = WL.GameOrderEvent.Create(Order.PlayerID, "Removed armies from "..Game.Map.Territories[t_id].Name,
-            {}, {terr_mod})
-        addNewOrder(order);
-        skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage);
+--        local terr_mod = WL.TerritoryModification.Create(t_id)
+--        terr_mod.AddArmies = -army_rm
+--        local order = WL.GameOrderEvent.Create(Order.PlayerID, "Removed armies from "..Game.Map.Territories[t_id].Name,
+--            {}, {terr_mod})
+--        addNewOrder(order);
+--        skipThisOrder(WL.ModOrderControl.SkipAndSupressSkippedMessage);
 
 
     end
