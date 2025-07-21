bf95052451a45dc50fe9f8f9738246079dfcd6af
stevewats0n <88929858+stevewats0n@users.noreply.github.com>
2025-07-21 09:44:17 +0100

    Update Client_SaveConfigureUI.lua

--- ArmyUpkeep/Client_SaveConfigureUI.lua
+++ ArmyUpkeep/Client_SaveConfigureUI.lua
@@ -4,8 +4,9 @@ require("Functions")
 function Client_SaveConfigureUI (alert)
   -- to resolve if user enters figure not to 0 or 1 dp
   local armyCost = Round( armyCostInput.GetValue() * 10 )
-  armyCost = armyCost / 10    
+  armyCost = armyCost / 10
   Mod.Settings.armyCost = armyCost
+  alert(armyCost)
   Mod.Settings.allowRemoveArmies = allowRemoveArmies.GetIsChecked()
   Mod.Settings.destroyArmies = destroyArmiesInput.GetIsChecked()
   Mod.Settings.setZeroArmiesNeutral = setZeroArmiesNeutral.GetIsChecked()
